aaef0531d2a2a642c96a66894dc6e6c07b04f346
Karla Sequen <karlasequen@gmail.com>
2022-08-08 17:01:59 -0600

    add html template to endpoint html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/blog")
+def blog():
+    return "This is a blog"
+
+
+@app.route("/html")
+def html():
+    return render_template('index.html')
